e2150cb63ad75f9cd12f6b0fd3945b161408f46a
martcoder <martcoder@martcoder.github>
2023-01-29 23:31:40 +0000

    Refactored construction of signals in C, was doing it wrong

--- Sem2/Assignment1/main.c
+++ Sem2/Assignment1/main.c
@@ -6,11 +6,12 @@
 
 #define signalLength 900
 #define idMultiplier 100
+#define idLength 9
 
 float * filteredSignal1;
 float * filteredSignal2;
 
-float FIR(float * weights, float * inputSignal, float * outputSignal){
+float doFIR(float * weights, float * inputSignal, float * outputSignal){
     int f = 0;
     int s = 0;
     for(f = 0; f < signalLength; f++){ // for every y value output
@@ -41,50 +42,55 @@ int main(void) {
     float student2[9] = {2.0f,0.0f,2.0f,2.0f,6.0f,7.0f,3.0f,4.0f,8.0f};
     float * signal1;
     float * signal2;
-    signal1 = (float*) malloc( sizeof(float) * (signalLength + N_FIR_B1) );
-    signal2 = (float*) malloc( sizeof(float) * (signalLength + N_FIR_B2) );
+    signal1 = (float*) malloc( sizeof(float) * signalLength );
+    signal2 = (float*) malloc( sizeof(float) * signalLength );
+
     
     FILE * fp1; // file pointer
     FILE * fp2; // file pointer
     fp1 = fopen("signal1.data","w"); //open file to write to
     fp2 = fopen("signal2.data","w"); //open file to write to
     
+    // Construct full 900 length signal1 and signal2
     int c = 0;
     int nine = 0;
-    for(c=0;c<idMultiplier;c++){
-        for(nine = 0; nine < 9; nine++){
-           signal1[c] = student1[nine];
-           fprintf(fp1,"%f\n",signal1[c]); //print signal1 value to file
-           signal2[c] = student2[nine]; 
-           fprintf(fp2,"%f\n",signal2[c]); //print signal2 value to file
+    printf("before signal construction");
+    int i = 0;
+    while(c < idMultiplier){ //100 times
+        for(nine = 0; nine < idLength; nine++){
+           signal1[i+nine] = student1[nine];
+           fprintf(fp1,"%f\n",signal1[i+nine]); //print signal1 value to file
+           signal2[i+nine] = student2[nine]; 
+           fprintf(fp2,"%f\n",signal2[i+nine]); //print signal2 value to file
         }
+        c++;
+        i = i + 9;
     }
     
-    int m = 0;
-    //append zeros
-    for(m = signalLength; m < (signalLength+N_FIR_B1); m++){
-        signal1[m] = 0.0f;
+    printf("after signal construction");
+
+    fclose(fp1);
+    fclose(fp2);
+    i = 0;
+    for(i = 0; i < 10; i++){
+      printf("sig1 and sig2 first 10 vals %f %f",signal1[i],signal2[i]);
     }
-    for(m = signalLength; m < (signalLength+N_FIR_B2); m++){
-        signal2[m] = 0.0f;
-    }
-    
     //
     filteredSignal1 = (float*) malloc(sizeof(float) * signalLength);
     filteredSignal2 = (float*) malloc(sizeof(float) * signalLength);
     
     int delay = 0;
-    //printf("Our weight value is %f and sig val is %f",b_fir1[0],signal1[1]);
+    printf("Our weight value is %f and sig1 val is %f ",b_fir1[0],signal1[0]);
 
-    //        scanf("%d",&delay);
     
-    FIR(b_fir1, signal1,filteredSignal1);
+    /*
+    doFIR(b_fir1, signal1,filteredSignal1);
     
     writeDataToFile("filtered1.data",filteredSignal1, signalLength);
     
-    FIR(b_fir2, signal2,filteredSignal2);
+    doFIR(b_fir2, signal2,filteredSignal2);
     
     writeDataToFile("filtered2.data",filteredSignal2, signalLength);
-    
+    */
 	return 0;
 }
